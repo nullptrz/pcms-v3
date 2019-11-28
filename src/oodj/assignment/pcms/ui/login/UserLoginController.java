e4efd5c40e43d208452a11bd2e492599f2566b3c
nullptrz <ahusuncode@gmail.com>
2019-11-29 07:48:53 +0800

    Initial Commit

---
+++ src/oodj/assignment/pcms/ui/login/UserLoginController.java
@@ -0,0 +1,47 @@
+package assignment.pcms.ui.login;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class UserLoginController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private JFXTextField username;
+
+    @FXML
+    private JFXPasswordField password;
+
+    @FXML
+    private JFXButton loginButton;
+
+    @FXML
+    void login(ActionEvent event) {
+        String user = username.getText();
+        String pass = password.getText();
+        if(user.equals("admin") && pass.equals("admin")){
+            System.out.println("Welcome admin");
+        }
+        else {
+            System.out.println("Wrong username or password");
+        }
+    }
+
+    @FXML
+    void initialize() {
+        assert username != null : "fx:id=\"username\" was not injected: check your FXML file 'user_login.fxml'.";
+        assert password != null : "fx:id=\"password\" was not injected: check your FXML file 'user_login.fxml'.";
+        assert loginButton != null : "fx:id=\"loginButton\" was not injected: check your FXML file 'user_login.fxml'.";
+
+    }
+}
